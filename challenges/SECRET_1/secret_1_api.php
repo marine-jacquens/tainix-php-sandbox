b44abb6ff6f38e49406d3510df50eece4a7c2b18
marine-itc <marine@itconsulting-solutions.com>
2023-02-21 11:42:32 +0100

    SECRET_1 challenge terminé

--- challenges/SECRET_1/secret_1_api.php
+++ challenges/SECRET_1/secret_1_api.php
@@ -20,8 +20,39 @@ foreach ($data as $name => $value) {
 
 // CODE DU CHALLENGE ------------------
 
+$alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+$countAlphabet = count($alphabet);
+$word = str_split($mot_crypte);
+$mot_decrypte = [];
 
+foreach ($word as $letter) {
+
+    for ($i = 0; $i < $countAlphabet; $i++) {
+
+        if ($alphabet[$i] == $letter) {
+
+            if ($i-$decalage >= $countAlphabet) {
+                
+               $index = ($i-$decalage) - $countAlphabet;
+               array_push($mot_decrypte, $alphabet[$index]);
+
+            }elseif ($i-$decalage < 0) {
+
+                $index = $countAlphabet + ($i-$decalage);
+                array_push($mot_decrypte, $alphabet[$index]);
+
+            }else {
+                array_push($mot_decrypte, $alphabet[$i-$decalage]);
+            }
+
+        }
+
+    }
+
+}
+
+$reponse = implode($mot_decrypte);
 
 
 // REPONSE ----------------------------
-// $game->output(['data' => ...]);
+$game->output(['data' => $reponse]);
--- challenges/SECRET_1/secret_1_local.php
+++ challenges/SECRET_1/secret_1_local.php
@@ -12,6 +12,40 @@ Html::debug($mot_crypte, '$mot_crypte');
 
 // CODE DU CHALLENGE ------------------
 
+$alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+$countAlphabet = count($alphabet);
+$word = str_split($mot_crypte);
+$mot_decrypte = [];
+
+foreach ($word as $letter) {
+
+    for ($i = 0; $i < $countAlphabet; $i++) {
+
+        if ($alphabet[$i] == $letter) {
+
+            if ($i-$decalage >= $countAlphabet) {
+                
+               $index = ($i-$decalage) - $countAlphabet;
+               array_push($mot_decrypte, $alphabet[$index]);
+
+            }elseif ($i-$decalage < 0) {
+
+                $index = $countAlphabet + ($i-$decalage);
+                array_push($mot_decrypte, $alphabet[$index]);
+
+            }else {
+                array_push($mot_decrypte, $alphabet[$i-$decalage]);
+            }
+
+        }
+
+    }
+
+}
+
+$reponse = implode($mot_decrypte);
+
+Html::debug($reponse);
 
 
 
