a087db3dc1c6f36c19666ad9600218274dad9b8e
marine-itc <marine@itconsulting-solutions.com>
2023-02-24 11:03:58 +0100

    crypto_2 challenge terminé

--- challenges/CRYPTO_2/crypto_2_api.php
+++ challenges/CRYPTO_2/crypto_2_api.php
@@ -20,8 +20,46 @@ foreach ($data as $name => $value) {
 
 // CODE DU CHALLENGE ------------------
 
+$reponse = $depart;
 
+foreach ($chemin as $element) {
+
+    $element_size = strlen($element);
+
+    $operator_number = 0;
+    $operator_type = "";
+
+    for ($i = 0; $i < $element_size; $i++) {
+
+        if ($element[$i] == "+") {
+
+            $operator_type = "+";
+            $operator_number++ ;
+
+        }else {
+
+            $operator_type = "-";
+            $operator_number++ ;
+
+        }
+
+    }
+
+    if ($operator_type == "+") {
+
+        $reponse += 1 . str_repeat(0, $operator_number-1);
+
+    }else {
+
+        $reponse -= 1 . str_repeat(0, $operator_number-1);
+
+    }
+
+
+}
+
+Html::debug($reponse);
 
 
 // REPONSE ----------------------------
-// $game->output(['data' => ...]);
+$game->output(['data' => $reponse]);
