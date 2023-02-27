cd10ab7f03e2584ca06d71dda66847f2df0443f9
marine-itc <marine@itconsulting-solutions.com>
2023-02-27 15:52:30 +0100

    security_2 challenge terminé

--- challenges/SECURITY_2/security_2_api.php
+++ challenges/SECURITY_2/security_2_api.php
@@ -20,8 +20,53 @@ foreach ($data as $name => $value) {
 
 // CODE DU CHALLENGE ------------------
 
+$alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+$alphabet_size = count($alphabet);
+$index = [];
+$crypto_word = "";
+$reponse = [];
 
+foreach ($words as $word) {
+
+    $word_to_array = str_split($word);
+
+    foreach ($word_to_array as $letter) {
+
+        foreach ($alphabet as $key => $value) {
+
+            if ($letter == $value) {
+
+                array_push($index, ($key * $keys[0] + $keys[1]) % $alphabet_size);
+
+            }
+
+        }
+
+    }
+
+    foreach ($index as $chiffrement) {
+
+        foreach ($alphabet as $key => $value) {
+
+            if ($chiffrement == $key) {
+
+                $crypto_word .= $alphabet[$key];
+
+            }
+        }
+
+
+    }
+
+    array_push($reponse, $crypto_word);
+
+    $crypto_word = "";
+    $index = [];
+
+}
+
+Html::debug(implode("-", $reponse));
 
 
 // REPONSE ----------------------------
-// $game->output(['data' => ...]);
+$game->output(['data' => implode("-", $reponse)]);
