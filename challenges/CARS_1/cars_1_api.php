b996d48645260ef87bfeda5b0768348789b26f3a
marine-itc <marine@itconsulting-solutions.com>
2023-02-28 17:35:09 +0100

    cars_1 challenge completed

--- challenges/CARS_1/cars_1_api.php
+++ challenges/CARS_1/cars_1_api.php
@@ -20,8 +20,45 @@ foreach ($data as $name => $value) {
 
 // CODE DU CHALLENGE ------------------
 
+$points = [];
+$result = 0;
 
+foreach ($plates as $plate) {
+
+    $symmetry = 0;
+
+    [$first_part, $second_part, $third_part] = preg_split("/-/", $plate);
+    
+    if ($first_part[1] == $third_part[0]) {
+
+        $symmetry++;
+
+    }
+
+    if ($first_part[0] == $third_part[1]) {
+
+        $symmetry++;
+
+    }
+
+    if ($second_part[0] == $second_part[2]) {
+        
+        $symmetry++;
+
+    }
+
+    $symmetry > 0 ? array_push($points, 1 . str_repeat(0, $symmetry)) : array_push($points, 1);
+
+}
+
+foreach ($points as $point) {
+
+    $result += $point;
+
+}
+
+Html::debug($result);
 
 
 // REPONSE ----------------------------
-// $game->output(['data' => ...]);
+$game->output(['data' => $result]);
--- challenges/CARS_1/cars_1_local.php
+++ challenges/CARS_1/cars_1_local.php
@@ -9,9 +9,44 @@ $plates = ['BM-822-KL', 'MV-383-TS', 'OY-918-FI', 'UN-899-PL', 'DX-154-SX', 'QW-
 Html::debug($plates, '$plates');
 
 // CODE DU CHALLENGE ------------------
+$points = [];
+$result = 0;
 
+foreach ($plates as $plate) {
 
+    $symmetry = 0;
 
+    [$first_part, $second_part, $third_part] = preg_split("/-/", $plate);
+    
+    if ($first_part[1] == $third_part[0]) {
+
+        $symmetry++;
+
+    }
+
+    if ($first_part[0] == $third_part[1]) {
+
+        $symmetry++;
+
+    }
+
+    if ($second_part[0] == $second_part[2]) {
+        
+        $symmetry++;
+
+    }
+
+    $symmetry > 0 ? array_push($points, 1 . str_repeat(0, $symmetry)) : array_push($points, 1);
+
+}
+
+foreach ($points as $point) {
+
+    $result += $point;
+
+}
+
+Html::debug($result);
 
 // REPONSE ATTENDUE -------------------
 Html::debug('228', 'Réponse attendue', 'success');
